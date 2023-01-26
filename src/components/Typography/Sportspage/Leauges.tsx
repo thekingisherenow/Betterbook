00c9146519b309485db8d49dc23bc8c01babb7f3
thekingisherenow <prajanbhattarai63@gmail.com>
2023-01-26 10:59:56 +0545

    one unwanted page soccer.tsx file removed.

--- src/components/Typography/Sportspage/Leauges.tsx
+++ src/components/Typography/Sportspage/Leauges.tsx
@@ -6,25 +6,25 @@ function Leauges() {
         <>
             <h1 className='mt-4 mb-2 font-semibold'> Soccer Leauges</h1>
 
-            <div className='flex flex-col h-[270px] space-y-1 border border-gray-600 text-xs rounded-xl'>
-                <div className='flex space-x-3 lg:space-x-6 cursor-pointer text-center py-2 mx-auto'>
+            <div className='flex flex-col h-[250px] space-y-1 border border-gray-600 text-xs rounded-xl'>
+                <div className='flex space-x-3 lg:space-x-6 cursor-pointer  py-2 '>
                     <div className='relative w-8 h-8 ml-2 lg:ml-4'>
                         <Image className='object-cover' sizes='8vw'
                             src={"https://iili.io/Hli55Vs.png"} fill alt='favicon' />
                     </div>
-                    <h1 className='text-center my-auto flex-grow'>Premiere Leauge</h1>
-                    <h1 className='text-center my-auto px-3'>5</h1>
+                    <h1 className='my-auto  text-left flex-grow'>Premiere Leauge</h1>
+                    <h1 className='text-left px-5 my-auto '>5</h1>
                 </div>
 
                 <hr className='border-gray-600' />
 
-                <div className='flex space-x-3 lg:space-x-6 cursor-pointer text-center py-2 mx-auto'>
+                <div className='flex space-x-3 lg:space-x-6 cursor-pointer  py-2 '>
                     <div className='relative w-8 h-8 ml-2 lg:ml-4'>
                         <Image className='object-cover' sizes='8vw'
-                            src={"https://iili.io/Hli55Vs.png"} fill alt='favicon' />
+                            src={"https://iili.io/H0AC771.png"} fill alt='laliga-icon' />
                     </div>
-                    <h1 className='text-center my-auto flex-grow'>Premiere Leauge</h1>
-                    <h1 className='text-center my-auto px-3'>5</h1>
+                    <h1 className=' my-auto text-left  flex-grow'>La liga</h1>
+                    <h1 className='text-left px-5 my-auto '>5</h1>
                 </div>
 
                 <hr className='border-gray-600' />
